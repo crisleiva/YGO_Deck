9b79db3a021cd17ad10bb5ebe9e44983907e40cc
Cristian Leiva <crisjosleiva@gmail.com>
2019-12-15 01:46:28 -0500

    added a func to store images locally

--- backend/ygo/api_communicator.py
+++ backend/ygo/api_communicator.py
@@ -1,6 +1,7 @@
 import urllib3
+import requests
 import json
-
+import os
 
 YGO_URL = "https://db.ygoprodeck.com/api/v5/cardinfo.php"
 http = urllib3.PoolManager()
@@ -8,15 +9,23 @@ ygo_cards = http.request('GET', YGO_URL)
 ygo_json = json.loads(ygo_cards.data.decode('UTF-8'))
 
 def find_img_link(data):
+	print("Images are being returned")
+	array = []
 	for dic in data:
 		for key in dic:
 			if key == "card_images":
 				for image in dic[key]:
 					for url in image:
 						if url == "image_url" or url == "image_url_small":
-							print("Images are being returned")
-							return image[url]
+							array.append(image[url])
+	return array
 
-def store_image(image)
+def store_image(url):
+	print("Download starting...")
+	for text in url:
+		size = text.split('/')[4]
+		f_ext = text.split('/')[-1]
+		print(size, f_ext)
 
-find_img_link(ygo_json)
+store_image(find_img_link(ygo_json))
+
