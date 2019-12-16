4815265716cf954df6451289698f55773a76a4bd
Cristian Leiva <crisjosleiva@gmail.com>
2019-12-15 22:53:31 -0500

    Added some routes to search for certain parameters

--- backend/ygo/api_communicator.py
+++ backend/ygo/api_communicator.py
@@ -3,6 +3,7 @@ import urllib.request
 import time
 import json
 import os
+from flask import jsonify
 #This file handles our api. We make requests to the url below and return it as json.
 YGO_URL = "https://db.ygoprodeck.com/api/v5/cardinfo.php"
 http = urllib3.PoolManager()
@@ -11,7 +12,7 @@ def api_call():
 	time.sleep(6)
 	ygo_cards = http.request('GET', YGO_URL)
 	ygo_json = json.loads(ygo_cards.data.decode('UTF-8'))
-	return ygo_json
+	return jsonify(ygo_json)
 
 #The code below takes in the json and returns only the image links provided with each card.	
 def find_img_link(data):
@@ -36,4 +37,24 @@ def store_image(url):
 		print(f"Starting new download for image {f_name}....")
 		urllib.request.urlretrieve(text, full_path)
 
-store_image(find_img_link(api_call()))
+#DRY code that takes the url and sends a get request and parses as json.
+def card_api(url):
+	ygo_card = http.request('GET', url)
+	ygo_json = json.loads(ygo_card.data.decode('UTF-8'))
+	return jsonify(ygo_json)
+
+#The code below is to find a certain card or cards by the parameter.
+def find_by_name(name):
+	y_name = name.replace(" ", "_")
+	name_url = YGO_URL + '?name=' + y_name
+	return card_api(name_url)
+
+def find_by_level(level):
+	level_url = YGO_URL + '?level=' + level
+	return card_api(level_url)
+
+def find_cards_by_archetype(arch):
+	a_name = arch.replace(" ", "-")
+	arch_url = YGO_URL + '?archetype=' + a_name
+	return card_api(arch_url)
+
--- backend/ygo/views.py
+++ backend/ygo/views.py
@@ -1,5 +1,4 @@
 from ygo import app, api_communicator
-from flask import jsonify
 
 @app.route('/', methods=['GET'])
 def index():
@@ -8,3 +7,16 @@ def index():
 @app.route('/api/v1/cards/all', methods=['GET'])
 def all_cards():
 	return api_communicator.api_call()
+
+@app.route('/api/v1/card/name/<name>', methods=['GET'])
+def find_card_by_name(name):
+	return api_communicator.find_by_name(name)
+
+
+@app.route('/api/v1/card/level/<level>', methods=['GET'])
+def find_card_by_level(level):
+	return api_communicator.find_by_level(level)
+
+@app.route('/api/v1/card/arch/<arch>', methods=['GET'])
+def find_by_arch(arch):
+	return api_communicator.find_cards_by_archetype(arch)
