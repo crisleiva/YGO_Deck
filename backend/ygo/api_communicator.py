fcefb0fcd4d953865f1c560f50ebafcd1c38cefb
Cristian Leiva <crisjosleiva@gmail.com>
2019-12-15 20:52:49 -0500

    Downloaded images from api to local folder

--- backend/ygo/api_communicator.py
+++ backend/ygo/api_communicator.py
@@ -1,13 +1,19 @@
 import urllib3
-import requests
+import urllib.request
+import time
 import json
 import os
-
+#This file handles our api. We make requests to the url below and return it as json.
 YGO_URL = "https://db.ygoprodeck.com/api/v5/cardinfo.php"
 http = urllib3.PoolManager()
-ygo_cards = http.request('GET', YGO_URL)
-ygo_json = json.loads(ygo_cards.data.decode('UTF-8'))
 
+def api_call():
+	time.sleep(6)
+	ygo_cards = http.request('GET', YGO_URL)
+	ygo_json = json.loads(ygo_cards.data.decode('UTF-8'))
+	return ygo_json
+
+#The code below takes in the json and returns only the image links provided with each card.	
 def find_img_link(data):
 	print("Images are being returned")
 	array = []
@@ -19,13 +25,15 @@ def find_img_link(data):
 						if url == "image_url" or url == "image_url_small":
 							array.append(image[url])
 	return array
-
+#The code store_image will take in each url and download it into a folder to store each picture locally.
 def store_image(url):
 	print("Download starting...")
+	file_path = "/Users/cristianleiva/Development/YGODeck/backend/ygo/images"
 	for text in url:
 		size = text.split('/')[4]
-		f_ext = text.split('/')[-1]
-		print(size, f_ext)
+		f_name = text.split('/')[-1]
+		full_path = file_path + '/' + size + '/' + f_name
+		print(f"Starting new download for image {f_name}....")
+		urllib.request.urlretrieve(text, full_path)
 
-store_image(find_img_link(ygo_json))
-
+store_image(find_img_link(api_call()))
